ac2918b32a29f831b46ba3590463cf4e11cec571
Alex Stanuta <alex.stanuta@gmail.com>
2020-07-14 12:31:23 +0300

    Create a script with functions that send an e-mail

---
+++ email_send_function.py
@@ -0,0 +1,33 @@
+#Script for sending an email with the smtplib library
+
+#emails.py
+#!/usr/bin/env python3
+
+import email.message
+import mimetypes
+import os.path
+import smtplib
+
+def generate_email(sender, recipient, subject, body, attachment_path = None):
+	message = email.message.EmailMessage()
+	message["From"] = sender
+	message["To"] = recipient
+	message["Subject"] = subject
+	message.set_content(body)
+
+	if attachment_path:
+		attachment_filename = os.path.basename(attachment_path)
+		mime_type, _ = mimetypes.guess_type(attachment_path)
+		mime_type, mime_subtype = mime_type.split('/', 1)
+
+		with open(attachment_path, 'rb') as ap:
+			message.add_attachment(ap.read(),
+									maintype = mime_type,
+									subtype = mime_subtype,
+									filename = attachment_filename)
+	return message
+
+def send_email(message):
+	mail_server = smtplib.SMTP('localhost')
+	mail_server.send_message(message)
+	mail_server.quit()
